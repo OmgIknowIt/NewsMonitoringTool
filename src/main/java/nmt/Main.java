f4b86b82033663ac0ea5cf341eb8ca095f2724db
Vladislavs Lapackis <vlapackis@gmail.com>
2019-03-19 16:03:49 +0200

    added new main class "Main" + class "Reader" slightly changed

---
+++ src/main/java/nmt/Main.java
@@ -0,0 +1,12 @@
+package nmt;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Reader read = new Reader();
+	}
+
+}
--- src/main/java/nmt/Reader.java
+++ src/main/java/nmt/Reader.java
@@ -3,19 +3,28 @@ package nmt;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-//import java.util.Iterator;
+import java.util.Iterator;
 
 import org.json.simple.JSONArray;
-//import org.json.simple.JSONObject;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
 public class Reader {
-	private static JSONArray hostList;
+	
+	private JSONArray hostList;
+	
+	public Reader() {
+		super();
+		read();
+	}
+	
 	@SuppressWarnings("unchecked")
-	public static void main (String[] args) {
+	private void read() {
 		JSONParser data = new JSONParser();
-        try (FileReader reader = new FileReader("/home/student/git/NewsMonitoringTools/src/main/java/nmt/source_urls.json"))
+        try (FileReader reader = new FileReader(
+        		"/home/student/git/NewsMonitoringTools/src/main/java/nmt/source_urls.json"))
         {
             //Read JSON file
             Object obj = data.parse(reader);
@@ -28,15 +37,19 @@ public class Reader {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (org.json.simple.parser.ParseException e) {
+        } catch (ParseException e) {
             e.printStackTrace();
         }
+	}
+	
+	public JSONArray getJsonData(){
+		return hostList;
 	}
 	
 //    private static void parseData(JSONObject data)
 //    {
 //    	String host = (String) data.get("host");
-//        JSONArray urls = (JSONArray) data.get("source");
+//        JSONArray urls = (JSONArray) data.get("url_list");
 //
 //        System.out.println("Name: " + host);
 //        System.out.println("\nUrls:");
