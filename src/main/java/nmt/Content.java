3b780ea2770a53bda4ac39b42869b0119f918b80
Vladislavs Lapackis <vlapackis@gmail.com>
2019-03-20 15:33:01 +0200

    files updated: Connection, Content, Main, Reader, .json and pom(again)

--- src/main/java/nmt/Content.java
+++ src/main/java/nmt/Content.java
@@ -1,5 +1,6 @@
 package nmt;
 
+import java.io.IOException;
 import java.util.Date;
 
 import javax.persistence.Entity;
@@ -9,19 +10,46 @@ import javax.persistence.PrePersist;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import org.json.simple.JSONArray;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 @Entity
 public class Content {
-	@Id@
-	GeneratedValue
+	@Id
+	@GeneratedValue
 	private Integer id;
-	
+
 	private String URL;
 	private String Title;
 	private String Source;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date;
-	
+
+	public Content(StringBuffer content, String source) throws IOException {
+		getContent(content);
+		this.Source = source;
+	}
+
+	private void getContent(StringBuffer content) throws IOException {
+		Document doc = Jsoup.parse(content.toString());
+		Elements links = doc.select("a.list-article__url");
+		for (Element link : links) {
+			Elements title = link.select("span.list-article__headline");
+			title.select(".list-article__comment").remove();
+			print(" * a: <%s>  (%s)", link.attr("abs:href"), title.text());
+			//TODO: send to DB
+			
+		}
+	}
+
+	private static void print(String msg, Object... args) {
+		System.out.println(String.format(msg, args));
+	}
+
 	@PrePersist
 	protected void onPrePersist() {
 		date = new Date();
