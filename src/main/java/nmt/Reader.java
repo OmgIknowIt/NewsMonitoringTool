b15818c4f88cf256bf6d633002415c4c713eda54
Evgeny Shulgin <evgshul@gmail.com>
2019-03-20 14:45:34 +0200

    add reader.java

--- src/main/java/nmt/Reader.java
+++ src/main/java/nmt/Reader.java
@@ -23,9 +23,9 @@ public class Reader {
 	@SuppressWarnings("unchecked")
 	private void read() {
 		JSONParser data = new JSONParser();
-        try (FileReader reader = new FileReader(
-        		"/home/student/git/NewsMonitoringTools/src/main/java/nmt/source_urls.json"))
-        {
+        try {FileReader reader = new FileReader(
+        		"/home/student/git/NewsMonitoringTools/src/main/java/nmt/source_urls.json");
+        
             //Read JSON file
             Object obj = data.parse(reader);
             
