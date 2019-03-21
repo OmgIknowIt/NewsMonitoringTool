3ee27f25cd21a30885f40b8ce52f50ef3ccfcb53
Rihards Dievapeds <r.dievapeds@gmail.com>
2019-03-21 09:43:25 +0200

    Duplicate entry validation

--- src/main/java/nmt/DBConnection.java
+++ src/main/java/nmt/DBConnection.java
@@ -4,32 +4,39 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 public class DBConnection {
 	EntityManagerFactory emf;
 	EntityManager em;
 	EntityTransaction entityTransaction;
 	
+	// initializing entityManager
 	public DBConnection() {
 		emf = Persistence.createEntityManagerFactory("db_news");
 		em = emf.createEntityManager();
 		entityTransaction = em.getTransaction();
 	}
 	
+	// Creates entries into DB
 	public void createEntry(String URL, String Title, String Source) {
 		Content newsEntry = new Content();
 		newsEntry.setURL(URL);
 		newsEntry.setTitle(Title);
 		newsEntry.setSource(Source);
-		em.getTransaction().begin();
-		em.persist(newsEntry);
-		em.getTransaction().commit();
-	}
-	
+		
+		// Check if entry already exists
+		entityTransaction.begin();		
+		Query query = em.createQuery("Select url from Content c where url = '" + URL + "'");
+		entityTransaction.commit();	
+		
+		// Persist entry if doesn't exist
+		if (query.getResultList().size() == 0) {
+			entityTransaction.begin();
+			em.persist(newsEntry);
+			entityTransaction.commit();
+		}
 
-	public static void main (String[] args){
-		DBConnection conn = new DBConnection();
-		conn.createEntry("https://tvnet.lv/", "Jeļena kļuva par miljonāru", "tvnet.lv");
 	}
 	
 }
