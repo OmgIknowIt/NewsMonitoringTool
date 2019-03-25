276496ceae9039860ac4af4dbe872c8ea2273a71
Vladislavs Lapackis <vlapackis@gmail.com>
2019-03-25 14:07:34 +0200

    !Pom WARNING! Crawler now can parse rss feeds

--- src/main/java/nmt/Connection.java
+++ src/main/java/nmt/Connection.java
@@ -2,13 +2,21 @@ package nmt;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Iterator;
+import java.util.zip.GZIPInputStream;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.xml.sax.InputSource;
+
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.SyndFeedInput;
 
 public class Connection {
 
@@ -22,6 +30,8 @@ public class Connection {
 		HttpURLConnection con = null;
 		BufferedReader in = null;
 		InputStreamReader streamReader = null;
+		SyndFeed feed = null;
+	    InputStream is = null;
 		for (int i = 0; i < jsonData.size(); i++) { // we have multiple websites
 													// with links
 			// let's take links from array
@@ -31,30 +41,50 @@ public class Connection {
 			//
 			while (iterator.hasNext()) { // if we need parse more than one link
 											// from website
-				// connection itself
-				URL url = new URL(iterator.next());
-				con = (HttpURLConnection) url.openConnection();
-				con.setRequestMethod("GET");
-				con.setConnectTimeout(8000);
-				//
-				// Reading the Response on Failed Requests
-				int status = con.getResponseCode();
-				if (status > 299) {
-					streamReader = new InputStreamReader(con.getErrorStream());
-				} else {
-					streamReader = new InputStreamReader(con.getInputStream());
+				String[] token = iterator.next().split("::");
+				String whereToGo = token[1].trim();
+				String link = token[0].trim();
+				if (whereToGo.equals("url")) {
+					// connection itself
+					URL url = new URL(link);
+					con = (HttpURLConnection) url.openConnection();
+					con.setRequestMethod("GET");
+					con.setConnectTimeout(8000);
+					//
+					// Reading the Response on Failed Requests
+					int status = con.getResponseCode();
+					if (status > 299) {
+						streamReader = new InputStreamReader(con.getErrorStream());
+					} else {
+						streamReader = new InputStreamReader(con.getInputStream());
+					}
+					in = new BufferedReader(streamReader);
+					String inputLine;
+					htmlCode = new StringBuffer();
+					while ((inputLine = in.readLine()) != null) {
+						htmlCode.append(inputLine);
+					}
+					new Crawler(htmlCode, (String) obj.get("host"), (String) obj.get("url"), (String) obj.get("title"));
+				} else if (whereToGo.equals("rss")) {
+				    try {
+				        URLConnection openConnection = new URL(link).openConnection();
+				        is = new URL(link).openConnection().getInputStream();
+				        if("gzip".equals(openConnection.getContentEncoding())){
+				            is = new GZIPInputStream(is);
+				        }
+				        InputSource source = new InputSource(is);
+				        SyndFeedInput input = new SyndFeedInput();
+				        feed = input.build(source);
+						new Crawler(feed, (String) obj.get("host"));
+				    } catch (Exception e){
+				        e.printStackTrace();
+				    }
 				}
-				in = new BufferedReader(streamReader);
-				String inputLine;
-				htmlCode = new StringBuffer();
-				while ((inputLine = in.readLine()) != null) {
-					htmlCode.append(inputLine);
-				}
 				//
-				new Crawler(htmlCode, (String) obj.get("host"), (String) obj.get("url"), (String) obj.get("title"));
 			}
-			in.close(); // close BufferedReader
-			con.disconnect(); // close HttpURLConnection
+			if (is != null) is.close(); // close InputStream
+			if (in != null) in.close(); // close BufferedReader
+			if (in != null) con.disconnect(); // close HttpURLConnection
 		}
 	}
 
--- src/main/java/nmt/Crawler.java
+++ src/main/java/nmt/Crawler.java
@@ -1,18 +1,27 @@
 package nmt;
 
 import java.io.IOException;
+import java.util.List;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+
 public class Crawler {
 
 	public Crawler(StringBuffer content, String source, String selectUrl, String selectTitle) throws IOException {
-		getContent(content, source, selectUrl, selectTitle);
+		getContentHTML(content, source, selectUrl, selectTitle);
 	}
 
-	private void getContent(StringBuffer htmlCode, String source, String selectUrl, String selectTitle)
+	public Crawler(SyndFeed content, String source) throws IOException {
+		getContentFeed(content, source);
+	}
+
+	private void getContentHTML(StringBuffer htmlCode, String source, String selectUrl, String selectTitle)
 			throws IOException {
 		Document doc = Jsoup.parse(htmlCode.toString());
 		Elements links = null;
@@ -39,13 +48,29 @@ public class Crawler {
 				title = link.select(selectTitle);
 			}
 
-			print(" * a: <%s> (%s)", link.attr("abs:href"), title.text());
+			//print(" * a: <%s> (%s)", link.attr("abs:href"), title.text());
 			DBConnection db = new DBConnection();
 			db.createEntry(link.attr("abs:href"), title.text(), source);
+			db.em.close();
+			db.emf.close();
 		}
 	}
-
-	private static void print(String msg, Object... args) {
-		System.out.println(String.format(msg, args));
+	
+	private void getContentFeed(SyndFeed feed, String source) {
+        try {
+        	for (SyndEntry entry : (List<SyndEntry>)feed.getEntries()) {
+        		//print(" * a: <%s> (%s)", entry.getLink(), entry.getTitle());
+    			DBConnection db = new DBConnection();
+    			db.createEntry(entry.getLink(), entry.getTitle(), source);
+    			db.em.close();
+    			db.emf.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
+
+//	private static void print(String msg, Object... args) {
+//		System.out.println(String.format(msg, args));
+//	}
 }
